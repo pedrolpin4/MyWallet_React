feaf0624cb1add22af405486b6cc64557b5ffe53
Pedro Lucas <pedrolucaspina22@gmail.com>
2021-11-28 16:42:23 -0300

    feat: add a test environment variable

--- src/service/api.js
+++ src/service/api.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 const API = axios.create({
     baseURL: process.env.NODE_ENV === 'production' ? 
     "https://my-walletapp.herokuapp.com" :
-    "http://localhost:4000"
+    process.env.NODE_ENV === 'development' ?
+    "http://localhost:4000" : 
+    "http://localhost:4001"
     ,
 })
 
